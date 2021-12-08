68d1e940519a6687690eb4f26c5dd9faa109f6a1
Will <wiltonti12@gmail.com>
2021-12-08 20:40:32 -0300

    add mocks to cluster mode

--- mock.go
+++ mock.go
@@ -19,3 +19,19 @@ func (mock MockRedis) GetMessage(key string) (string, error) {
 
 	return args.Get(0).(string), args.Error(1)
 }
+
+type MockRedisCluster struct {
+	mock.Mock
+}
+
+func (mock MockRedisCluster) ClusterSetMessage(key string, message interface{}) error {
+	args := mock.Called(key, message)
+
+	return args.Error(0)
+}
+
+func (mock MockRedisCluster) ClusterGetMessage(key string) (string, error) {
+	args := mock.Called(key)
+
+	return args.Get(0).(string), args.Error(1)
+}
